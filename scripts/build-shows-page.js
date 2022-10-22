6b60f9fd4a5330621887721961f0f439549c8f5e
Akash Chauhan <akash.sc96@gmail.com>
2022-10-22 14:29:07 -0400

    Comment out the html structure for shows in the shows.html file and copy it into the the build-shows-page.js file to be used as reference for DOM and link the js file within the shows.html file

---
+++ scripts/build-shows-page.js
@@ -0,0 +1,36 @@
+{
+  /* <section class="shows-table">
+<div class="shows-table__container">
+  <h2 class="shows-table__title">Shows</h2>
+  <!-- This div container below will be hidden in mobile but will be display: block on tablet and desktop -->
+  <div class="tickets__column-headings--hide-mobile">
+    <p class="tickets__date-title">Date</p>
+    <p class="tickets__venue-title">Venue</p>
+    <p class="tickets__location-title">Location</p>
+    <a
+      href="./index.html"
+      class="tickets__buy-button tickets__buy-button--visibility-hidden"
+    >
+      Buy Tickets
+    </a>
+  </div>
+  <article class="tickets">
+    <p class="tickets__date-title tickets__date-title--hide-tablet">
+      Date
+    </p>
+    <p class="tickets__date">Mon Sept 06 2021</p>
+    <p class="tickets__venue-title tickets__venue-title--hide-tablet">
+      Venue
+    </p>
+    <p class="tickets__venue">Ronald Lane</p>
+    <p
+      class="tickets__location-title tickets__venue-title--hide-tablet"
+    >
+      Location
+    </p>
+    <p class="tickets__location">San Francisco, CA</p>
+    <a href="./index.html" class="tickets__buy-button">Buy Tickets</a>
+  </article>
+</div>
+</section> */
+}
