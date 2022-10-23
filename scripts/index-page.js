d86d31757b1bc8ac772ceee208a079c4840a29cf
Akash Chauhan <akash.sc96@gmail.com>
2022-10-23 15:12:36 -0400

    Link javascript file index-page.js to index.html and create array of the 3 comments to be added via DOM inside js file

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,3 +1,15 @@
+const comments = [
+  {
+    name: ["Connor Walton", "Emilie Beach", "Miles Acosta"],
+    date: ["02/17/2021", "01/09/2021", "12,20,2020"],
+    comment: [
+      "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
+      "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
+      "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
+    ],
+  },
+];
+
 /* <article class="comment-array">
 <div class="comment-array__avatar-container">
   <!-- <img
