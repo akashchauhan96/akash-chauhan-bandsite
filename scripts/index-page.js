0d53dab362cb426df70008a3304db2c5cd6329a3
Akash Chauhan <akash.sc96@gmail.com>
2022-10-23 15:05:13 -0400

    Add comment list html into index-page.js to be used as reference and comment out the html structure for the comment list in index.html

---
+++ scripts/index-page.js
@@ -0,0 +1,21 @@
+/* <article class="comment-array">
+<div class="comment-array__avatar-container">
+  <!-- <img
+    src="./assets/images/Mohan-muruge.jpg"
+    alt="avatar image of the person submitting comment"
+    class="comment-section__avatar-image"
+  /> -->
+</div>
+<div class="comment-array__container-no-image">
+  <div class="comment-array__name-date-wrapper">
+    <p class="comment-array__name">Connor Walton</p>
+    <p class="comment-array__date">02/17/2021</p>
+  </div>
+  <p class="comment-array__comment">
+    This is art. This is inexplicable magic expressed in the purest
+    way, everything that makes up this majestic work deserves
+    reverence. Let us appreciate this for what it is and what it
+    contains.
+  </p>
+</div>
+</article> */
